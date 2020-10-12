a8755c83cd8aa2e9f718a8cd90797d9557837a71
VegueCarlosR <72750826+VegueCarlosR@users.noreply.github.com>
2020-10-12 22:53:24 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,6 +1,6 @@
 # Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
-def my_email(e):
-    return e
-print("Carlos.Vegue@alu.uclm.es")
+
+def my_email():
+    return "Carlos.Vegue@alu.uclm.es"
    
